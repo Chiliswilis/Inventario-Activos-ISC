70ff49fd9097c3a583a09255c6e700e3006fc8ac
ProgrammerJunior1994 <br3400483@gmail.com>
2026-03-11 22:00:08 -0600

    Modifiacion de credenciales de supabase

--- backend/src/supabase.js
+++ backend/src/supabase.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const { createClient } = require("@supabase/supabase-js");
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseUrl = "https://roqrezpmkafeewfenpia.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvcXJlenBta2FmZWV3ZmVucGlhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MTUyMzc2NSwiZXhwIjoyMDg3MDk5NzY1fQ.1bC5kc4TFRCcbHLsdGprZSx-t5hw4_kqMnTNg5QL_og";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
