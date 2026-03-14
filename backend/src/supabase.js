46b8ed15b4756f1870ecc65e658b202680d78f30
Victor Daniel Sainz Preciado <victordanielsainz07@gmai.com>
2026-03-14 10:34:46 -0600

    fix: mover credenciales a .env

--- backend/src/supabase.js
+++ backend/src/supabase.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const { createClient } = require("@supabase/supabase-js");
 
-const supabaseUrl = "https://ktldlqvkjlfznzuqvkoh.supabase.co";
-const supabaseKey = "sb_secret_aNS2prljyY_Zp_I-wOVkGA_Q2n-cWs4";
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
